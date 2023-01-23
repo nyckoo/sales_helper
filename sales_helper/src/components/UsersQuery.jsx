4ec940a6c03e1756c45a8e3eabb2faab688f718a
mtyton <mateusz.tyton99@gmail.com>
2023-01-23 22:32:16 +0100

    small alyout changes

--- sales_helper/src/components/UsersQuery.jsx
+++ sales_helper/src/components/UsersQuery.jsx
@@ -2,47 +2,48 @@ import { staticUsers } from "../constants";
 import React, { useState, useEffect } from "react";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
 import styles from "../style";
-import { arrowUpRight } from "../assets";
+import {arrowDown, arrowUp} from "../assets";
 
-const UsersQuery = ({ pageNumber, searchedUsers, offerUuid }) => {
+const UsersQuery = ({offerUuid }) => {
     const [users, setUsers] = useState([]);
     const [searched, isSearched] = useState(false);
     const axiosPrivate = useAxiosPrivate();
+    const [page, setPage] = useState(1)
 
     useEffect(() => {
         const getUsers = async () => {
             try {
-                const url = `/offers/${offerUuid}/match?skip=${(pageNumber - 1) * 5}&limit=${pageNumber*5}`;
+                const url = `/offers/${offerUuid}/match?skip=${(page - 1) * 5}&limit=${page*5}`;
                 const { data } = await axiosPrivate.get(url);
                 console.log(data);
                 setUsers(data);
-                searchedUsers ? setUsers(searchedUsers) : setUsers(data);
             } catch (err) {
                 console.log(err);
             }
         };
         getUsers();
-    }, [pageNumber, searchedUsers]);
+    }, [page]);
     // {user.surname}{" "}{user.name}&nbsp;{match}
 
     return (
         <>
+            <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
+                Matching Employees: 
+            </h4>
             <div className="flex flex-col">
+                <div className={`${styles.flexCenter} flex-col w-[50px] h-[50px] rounded-full mb-5 ml-20`}>
+                    <img src={arrowUp} className="w-[50%] h-[50%] object-contain" onClick={() => setPage(page, -1)} />
+                </div>
                 {users.map((user, index) => (
                     <div className="items-center font-medium text-dimWhite flex flex-row mt-1" key={index}>
-                        <div className={`min-w-[40px] h-[30px] rounded-full ${styles.flexCenter} bg-extra-gradient`}>
-                            <img src={arrowUpRight} className="object-contain"
-                                onClick={() => isSearched(true)} />
-                        </div>
                         <p className="ml-2 text-oldWhite">
-                            {user.employee.name}{" "}{user.employee.surname}{" - Match: "}{user.match_ratio.toPrecision(2) * 100}{"%"}
-                            {/* searched &&  */}
-                            {/* <UserDetails isVisible={searched}
-                                offerUuid={offerUuid} /> */}
-
+                            {user.employee.name}{" "}{user.employee.surname}{" - Match: "}{(user.match_ratio * 100).toFixed(2)}{"%"}
                         </p>
                     </div>
                 ))}
+                <div className={`${styles.flexCenter} flex-col w-[50px] h-[50px] rounded-full mt-5 ml-20`}>
+                    <img src={arrowDown} className="w-[50%] h-[50%] object-contain" onClick={() => setPage(page + 1)} />
+                </div>
             </div>
         </>
     );
